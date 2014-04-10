58f18f46d273ac1259fb9b08401f5546bc7fd097
Pablo Vergel & Tony Ta <pair+pvergel.tonyta@devbootcamp.com>
2014-04-10 14:40:47 -0500

    Root added in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ProjectBernies::Application.routes.draw do
+  root 'welcome#index'
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
