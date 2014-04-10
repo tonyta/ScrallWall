c4547d5467a44c1796803c853a0d9142cf11ea9f
Pablo Vergel & Tony Ta <pair+pvergel.tonyta@devbootcamp.com>
2014-04-10 16:25:58 -0500

    adds controllers and their specs

---
+++ app/controllers/artifacts_controller.rb
@@ -0,0 +1,16 @@
+class ArtifactsController < ApplicationController
+  def index
+  end
+
+  def explore
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,12 @@
+class PicturesController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
