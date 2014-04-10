a22e9b2468f5d8be7a3c19012811cc796b68917e
Pablo Vergel & Tony Ta <pair+pvergel.tonyta@devbootcamp.com>
2014-04-09 20:22:26 -0500

    initial commit: adds rails structure with initial config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectBernies::Application.config.secret_key_base = '953c4fe320fdbf410747f1d972c624fb31532b27e46b7760406e1ee6eb54d0cae6e46ca9307f7e2fbd8a26a06572806910b3ea8ec69ff5aa9bcd7fb5ef8fab84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectBernies::Application.config.session_store :cookie_store, key: '_project_bernies_session'
