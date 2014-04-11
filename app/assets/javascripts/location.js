f87d934ea9453616dc5378aef1e86fce73daca87
Tony Ta <tonyta.tt@gmail.com>
2014-04-11 10:51:11 -0500

    temporary

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   var handler = Gmaps.build('Google', { markers: { clusterer: {gridSize: 50} } } );
 
   handler.buildMap({ provider: { maxZoom: 50 }, internal: {id: 'map'}}, function(){
@@ -7,17 +8,4 @@ $(document).ready(function() {
     handler.fitMapToBounds();
   });
 
-  $("#geolocation").on("click", function(e) {
-    e.preventDefault();
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      $("#latlong").append("Geolocation is not supported by this browser.");
-    }
-
-    function showPosition(position) {
-     $("#latlong").append("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
-     console.log("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
-    }
-  })
 });
