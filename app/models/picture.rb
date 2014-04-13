406c6383914ca74d0e50e3c1600de3a9e989dbe8
Greg Santikian <gsantikian@gmail.com>
2014-04-12 23:04:31 -0500

    updates picture and vote associations and vote_spec

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,7 +4,7 @@ class Picture < ActiveRecord::Base
   belongs_to :user
   belongs_to :artifact
 
-  has_many :votes, as: :votable
+  has_many :votes
   has_many :flags, as: :flaggable
 
   mount_uploader :image, ImageUploader
