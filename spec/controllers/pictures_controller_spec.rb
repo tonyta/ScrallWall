8b657e4c456939f2a61d7590d9892bcf86d4cb5f
Greg Santikian <gsantikian@gmail.com>
2014-04-14 21:46:04 -0500

    adds tests for artifacts, home, and pictures controllers

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -2,4 +2,22 @@ require 'spec_helper'
 
 describe PicturesController do
 
+  describe "#new" do
+    it "it should assign an empty post" do
+      picture = double(:picture)
+      Picture
+        .should_receive(:new)
+        .and_return(picture)
+      get :new
+      expect(assigns(:picture)).to eq picture
+    end
+  end
+
+  describe "#create" do
+    it "should redirect to new picture unless a picture is submitted" do
+      picture_params = { "picture" => { "image" => nil }}
+      post :create, picture_params
+      expect(response).to redirect_to(new_picture_path)
+    end
+  end
 end
