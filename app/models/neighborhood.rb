5017080496f9c1307546063b32d7dbea583f2ab6
Greg Santikian <gsantikian@gmail.com>
2014-04-15 13:15:02 -0500

    neighborhood stuff

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -6,4 +6,14 @@ class Neighborhood < ActiveRecord::Base
       )
     } % [longitude, latitude])
   }
+
+  scope :within, -> (latitude, longitude, neighborhood) {
+  where(%{
+    ST_Within(
+      ST_GeomFromText(
+        'SRID=4326;POINT(' || artifacts.longitude || ' ' || artifacts.latitude || ')'
+        ),
+      (select geom from neighborhoods where pri_neigh = '%s')
+    } % [neighborhood])
+  }
 end
