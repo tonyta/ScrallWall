561002fcd36ac6110d7657b40c12cd602fad8dd8
Greg Santikian <gsantikian@gmail.com>
2014-04-15 19:01:08 -0500

    Votes implemented. Ajax working for upvote/downvote. Needs to be added ajax for counter

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ ProjectBernies::Application.routes.draw do
     end
   end
 
+  resources :artifacts do
+    put "upvote", :to => "artifacts#upvote", as: :upvote
+    put "downvote", :to => "artifacts#downvote", as: :downvote
+  end
+
+  patch 'artifacts/:artifact_id', to: 'artifacts#update'
   patch 'artifacts/:artifact_id/pictures/:picture_id', to: 'pictures#update', as: 'artifact_picture'
   get 'pictures/neighbors', to: 'pictures#neighbors', constraints: {format: 'json'}
   get 'neighborhoods/find', to: 'neighborhoods#find', constraints: {format: 'json'}
