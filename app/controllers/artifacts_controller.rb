4145f49c0a5cc20902787b6662430f6fa5248ca7
Greg Santikian <gsantikian@gmail.com>
2014-04-15 19:01:08 -0500

    Votes implemented. Ajax working for upvote/downvote. Needs to be added ajax for counter

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -15,4 +15,27 @@ class ArtifactsController < ApplicationController
   def show
     @artifact = Artifact.find(params[:id])
   end
+
+  def update
+    # @artifact = Artifact.find(params[:id])
+    # if params[:vote] == 'upvote'
+    #   artifact.votes += 1
+    # elsif params[:vote] == 'downvote'
+    #   artifact.votes -= 1
+    # end
+  end
+
+  def upvote
+    @artifact = Artifact.find(params[:artifact_id])
+    @artifact.votes += 1
+    @artifact.save
+    render nothing: true
+  end
+
+  def downvote
+    @artifact = Artifact.find(params[:artifact_id])
+    @artifact.votes -= 1
+    @artifact.save
+    render nothing: true
+  end
 end
